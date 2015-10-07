a954435694a7813335cb83f3fdff97bbba3799fc
Pedro Zuppelli <pedro.zuppelli@hotmail.com>
2015-10-07 03:21:31 -0300

    Depreca clases de descuento.

--- cake-world-domain/src/main/java/com/zuppelli/cake/comercio/CarritoDescuentoCupon.java
+++ cake-world-domain/src/main/java/com/zuppelli/cake/comercio/CarritoDescuentoCupon.java
@@ -1,8 +1,12 @@
 package com.zuppelli.cake.comercio;
 
+import com.zuppelli.livingdocs.ReemplazadoPor;
+
 /**
- * ??
+ * Carrito con cupon de descuento.
  */
+@Deprecated
+@ReemplazadoPor( referencia = Carrito.class, razon = "Se aplica el patron Strategy en el carrito, por lo que esta clase queda redundante." )
 public class CarritoDescuentoCupon extends Carrito
 {
     private Cupon cupon;
--- cake-world-domain/src/main/java/com/zuppelli/cake/comercio/CarritoDescuentoPrimeraCompra.java
+++ cake-world-domain/src/main/java/com/zuppelli/cake/comercio/CarritoDescuentoPrimeraCompra.java
@@ -1,8 +1,12 @@
 package com.zuppelli.cake.comercio;
 
+import com.zuppelli.livingdocs.ReemplazadoPor;
+
 /**
  * ??AA
  */
+@Deprecated
+@ReemplazadoPor( referencia = Carrito.class, razon = "Se aplica el patron Strategy en el carrito, por lo que esta clase queda redundante." )
 public class CarritoDescuentoPrimeraCompra extends Carrito
 {
     public double getPrecio() {
